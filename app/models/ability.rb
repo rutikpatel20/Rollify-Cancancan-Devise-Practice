b9d05ae4170641c8f3f0671165621788caa74609
rutiksimform <rutik.p@simformsolutions.com>
2022-07-18 12:26:22 +0530

    add ability

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,6 +4,11 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
+    if user.has_role? :admin
+      can :manage, :all
+    else
+      can :read, :all
+    end
     # Define abilities for the user here. For example:
     #
     #   return unless user.present?
